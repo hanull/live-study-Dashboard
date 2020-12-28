f70e4132fe14ba9b4f07b48031ad34e5b904c617
hanull <dkfghg3@naver.com>
2020-12-29 02:35:54 +0900

    토큰 정보 삭제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        final String OAuthToken = "9a236c15b7230b5f75a597c117d311f61e8f8a50";
+        final String OAuthToken = "유저 토큰";
         final String repoPath = "whiteship/live-study";
 
         LiveStudyDashboard liveStudyDashboard = new LiveStudyDashboard(OAuthToken, repoPath);
